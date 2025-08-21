26d61b556ed2aa31142ecea2bc777d4ec0531b42
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-08-21 10:54:14 -0700

    testing last updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,41 +21,44 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
+  const commitDate = process.env.VERCEL_GIT_COMMIT_DATE;
+
   return (
     <html lang="en">
       <body className={`${lato.className}`}>
-              {/* Background */}
+        {/* Background */}
 
-      {/* Body */}
-      <div className="body">
-        {children}
-      </div>
-
-      {/* Header */}
-      {/* <div className={styles.header}>
-        <div>
-          updated 4/5
+        {/* Body */}
+        <div className="body">
+          {children}
         </div>
-      </div> */}
 
-      {/* Footer */}
-      <div className="footer">
-        <div className="footer-left">Thomas Yeung</div>
+        {/* Header */}
+        {/* <div className={styles.header}>
+          <div>
+            updated 4/5
+          </div>
+        </div> */}
 
-        <div className="footer-right">
-          <Link className="footer-link" href="https://github.com/tomayyeung" target="_blank">
-            <Image className="footer-img" src="icons/github.svg" alt="Github" width={25} height={25} />
-          </Link>
+        {/* Footer */}
+        <div className="footer">
+          <div className="footer-left">Thomas Yeung</div>
+          <div>Last updated: {commitDate ? new Date(commitDate).toDateString() : "Unknown"}</div>
 
-          <Link className="footer-link" href="https://www.linkedin.com/in/thomas-h-yeung/" target="_blank">
-            <Image className="footer-img" src="icons/linkedin.svg" alt="LinkedIn" width={25} height={25} />
-          </Link>
+          <div className="footer-right">
+            <Link className="footer-link" href="https://github.com/tomayyeung" target="_blank">
+              <Image className="footer-img" src="icons/github.svg" alt="Github" width={25} height={25} />
+            </Link>
 
-          <Link className="footer-link" href="https://leetcode.com/u/thyeung/" target="_blank">
-            <Image className="footer-img" src="icons/leetcode.svg" alt="LeetCode" width={25} height={25} />
-          </Link>
+            <Link className="footer-link" href="https://www.linkedin.com/in/thomas-h-yeung/" target="_blank">
+              <Image className="footer-img" src="icons/linkedin.svg" alt="LinkedIn" width={25} height={25} />
+            </Link>
+
+            <Link className="footer-link" href="https://leetcode.com/u/thyeung/" target="_blank">
+              <Image className="footer-img" src="icons/leetcode.svg" alt="LeetCode" width={25} height={25} />
+            </Link>
+          </div>
         </div>
-      </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
     <>
-      <h1>Thomas Yeung</h1>
-      <p>About</p>
-      <p>Projects</p>
+      <h1>Hey there! 😺</h1>
+      <p>I&apos;m Thomas, a second-year student at the University of San Francisco.</p>
+      <p>Some of my interests include:</p>
+      <ul>
+        <li>Game development</li>
+        <li>Artificial intelligence</li>
+        <li>Web development</li>
+      </ul>
+
+      <div style={{borderTop: "1px solid white", margin: "2em 0"}}></div>
+
+      <h2>A little more about me:</h2>
     </>
   )
 }
